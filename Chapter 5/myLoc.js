c027a2c53ecc4654b3600ea9c22fdd7faa3cb888
Cesare de Cal <hi@cesare.io>
2015-12-21 10:29:33 +0100

    In progress: geolocation exercise

--- Chapter 5/myLoc.js
+++ Chapter 5/myLoc.js
@@ -1,8 +1,37 @@
 window.onload = getMyLocation;
 
+
+var googleLatAndLong = new google.maps.LatLng(latitude, longitude);
+
+var mapOptions = {
+	zoom: 10,
+	center: googleLatAndLong,
+	mapTypeId: google.maps.MapTypeID.ROADMAP
+}
+
+var ourCoords = {
+	latitude: 47.624851,
+	longitude: -122.52099
+};
+
+var map;
+
+function showMap(coords) {
+	var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+	
+	var mapOptions = {
+		zoom: 10,
+		center: googleLatAndLong,
+		mapTypeId: google.maps.MapTypeID.ROADMAP
+	}
+	
+	var mapDiv = document.getElementById("map");
+	map = new google.maps.Map(mapDiv, mapOptions);
+}
+
 function getMyLocation() {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(displayLocation);
+		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
 	} else {
 		alert("Oops, no geolocation support");
 	}
@@ -11,7 +40,39 @@ function getMyLocation() {
 function displayLocation(position) {
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
-	
 	var div = document.getElementById("location");
 	div.innerHTML = "You are at Latitude: " + latitude + ", longitude: " + longitude;
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = document.getElementById("distance");
+	distance.innerHTML = "You are " + km + " km from WickedlySmart HQ";
+	
+	showMap(position.coords);
+}
+
+function displayError(error) {
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied by user",
+		2: "Position is not available",
+		3: "Request timed out"
+	};
+	var errorMessage = errorTypes[error.code];
+	if (error.code === 0 || error.code == 2) {
+		errorMessage = errorMessage + "" + error.message;
+	}
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
+}
+
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+	var radiusEarth = 6371;
+	return Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + Math.cos(startLatRads) * Math.cos(destLatRads) * Math.cos(startLongRads - destLongRads)) * radiusEarth;
+}
+
+function degreesToRadians(degrees) {
+	return degrees * Math.PI / 180;
 }
