59d778f336861dcc003da764912a6b81919f670f
Cesare de Cal <hi@cesare.io>
2015-12-22 16:16:52 +0100

    In progress: `mightyGumball` exercise

--- Chapter 6/Mighty Gumball/mightygumball.js
+++ Chapter 6/Mighty Gumball/mightygumball.js
@@ -1,1 +1,27 @@
 /* mightygumball.js */
+
+window.onload = function() {
+	setInterval(handleRefresh, 3000);
+}
+
+function handleRefresh() {
+	var url = "http://gumball.wickedlysmart.com/?callback=updateSales"                            
+
+	var newScriptElement = document.createElement("script");
+	newScriptElement.setAttribute("src", url);
+	newScriptElement.setAttribute("id", "jsonp");
+	
+	var oldScriptElement = document.getElementById("jsonp");
+	var head = document.getElementByTagName
+}
+
+function updateSales(sales) {
+	var salesDiv = document.getElementById("sales");
+	for (var i = 0; i < sales.length; i++) {
+		var sale = sales[i];
+		var div = document.createElement("div");
+		div.setAttribute("class", "saleItem");
+		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+		salesDiv.appendChild(div);
+	}
+}
