bcd776cd5633a54d44b9b1cb1f70ce04c759d6e0
Cesare de Cal <hi@cesare.io>
2015-12-23 11:24:38 +0100

    I wrote the drawSquare function

--- Chapter 7/tweetshirt.js
+++ Chapter 7/tweetshirt.js
@@ -1,12 +1,34 @@
 /* tweetshirt.js */
 
 window.onload = function() {
+	var button = document.getElementById("previewButton");
+	button.onclick = previewHandler;
+}
+
+function previewHandler() {
 	var canvas = document.getElementById("tshirtCanvas");
-	var context = canvas.getContext("2d");
 	
 	if (canvas.getContext) {
-		context.fillRect(10, 10, 100, 100);
+		var context = canvas.getContext("2d");
+		var selectObj = document.getElementById("shape");
+		var index = selectObj.selectedIndex;
+		var shape = selectObj[index].value;
+		
+		if (shape == "squares") {
+			for (var squares = 0; squares < 20; squares++) {
+				drawSquare(canvas, context);
+			}
+		}
+		
 	} else {
 		alert("Upgrade your browser to support canvas!");
-	}
+	}	
+}
+
+function drawSquare(canvas, context) {
+	var width = Math.floor(Math.random() * 40);
+	var x = Math.floor(Math.random() * canvas.width);
+	var y = Math.floor(Math.random() * canvas.height);
+	context.fillStyle = "lightblue";
+	context.fillRect(x, y, width, width);
 }
