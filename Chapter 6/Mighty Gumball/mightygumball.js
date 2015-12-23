d424f0b55a543fa2ed79527857291ed7eaa7c570
Cesare de Cal <hi@cesare.io>
2015-12-23 09:21:49 +0100

    Finished up the "Mighty Gumball" exercise

--- Chapter 6/Mighty Gumball/mightygumball.js
+++ Chapter 6/Mighty Gumball/mightygumball.js
@@ -5,15 +5,22 @@ window.onload = function() {
 }
 
 function handleRefresh() {
-	var url = "http://gumball.wickedlysmart.com/?callback=updateSales"                            
+	var url = "http://gumball.wickedlysmart.com/?callback=updateSales" + "&lastreporttime=" + lastReportTime + "&random=" + (new Date()).getTime();                          
 
 	var newScriptElement = document.createElement("script");
 	newScriptElement.setAttribute("src", url);
 	newScriptElement.setAttribute("id", "jsonp");
 	
 	var oldScriptElement = document.getElementById("jsonp");
-	var head = document.getElementByTagName
+	var head = document.getElementsByTagName("head")[0];
+	if (oldScriptElement == null) {
+		head.appendChild(newScriptElement);
+	} else {
+		head.replaceChild(newScriptElement, oldScriptElement);
+	}
 }
+
+var lastReportTime = 0;
 
 function updateSales(sales) {
 	var salesDiv = document.getElementById("sales");
@@ -24,4 +31,7 @@ function updateSales(sales) {
 		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
 		salesDiv.appendChild(div);
 	}
+	if (sales.length > 0) {
+		lastReportTime = sales[sales.length-1].time;
+	} 
 }
