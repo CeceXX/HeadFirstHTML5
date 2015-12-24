be4e2288f175fe40b22c24282b59bd0a82ebe05c
Cesare de Cal <hi@cesare.io>
2015-12-24 09:01:17 +0100

    getFormatExtension function

--- Chapter 8/webvilletv.js
+++ Chapter 8/webvilletv.js
@@ -22,3 +22,13 @@ function nextVideo() {
 	video.load();
 	video.play();
 }
+
+function getFormatExtension() {
+	if (video.canPlayType("video/mp4") != "") {
+		return ".mp4";
+	} else if (video.canPlayType("video/webm") != "") {
+		return ".webm";
+	} else if (video.canPlayType("video/ogg") != "") {
+		return ".ogv";
+	}
+}
