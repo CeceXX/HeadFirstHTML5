1dc293298ba9f68eca62346867c69d11afaadd41
Cesare de Cal <hi@cesare.io>
2015-12-27 12:22:26 +0100

    Added alert popping out

--- Chapter 9/notetoself.js
+++ Chapter 9/notetoself.js
@@ -1,1 +1,7 @@
 /* notetoself.js */
+
+localStorage.setItem("sticky_0", "Pick up dry cleaning");
+localStorage.setItem("sticky_1", "Cancel cable tv, who needs it now?");
+
+var sticky = localStorage.getItem("sticky_0");
+alert(sticky);
