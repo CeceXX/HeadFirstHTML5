53ec5d661032a5f85d993910b8698e4a66eb8458
Cesare de Cal <hi@cesare.io>
2015-12-27 20:28:58 +0100

    Completed "The Shell Game"

--- Chapter 9/notetoself.js
+++ Chapter 9/notetoself.js
@@ -3,5 +3,8 @@
 localStorage.setItem("sticky_0", "Pick up dry cleaning");
 localStorage.setItem("sticky_1", "Cancel cable tv, who needs it now?");
 
-var sticky = localStorage.getItem("sticky_0");
-alert(sticky);
+for (var i = 0; i < localStorage.length; i++) {
+	var key = localStorage.key(i);
+	var value = localStorage[key];
+	alert(value);
+}
