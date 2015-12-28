66c1cbde3f73dd18dabf50a5f682929f8fee7c1b
Cesare de Cal <hi@cesare.io>
2015-12-28 09:03:41 +0100

    Added addStickyToDOM function

--- Chapter 9/notetoself.js
+++ Chapter 9/notetoself.js
@@ -11,5 +11,15 @@ window.onload = function() {
 	}	
 }
 
+function addStickyToDOM(value) {
+	var stickies = document.getElementById("stickies");
+	var sticky = document.createElement("li");
+	var span = document.createElement("span");
+	span.setAttribute("class", "sticky");
+	span.innerHTML = value;
+	sticky.appendChild(span);
+	stickies.appendChild(sticky);	
+}
+
 localStorage.setItem("sticky_0", "Pick up dry cleaning");
 localStorage.setItem("sticky_1", "Cancel cable tv, who needs it now?");
