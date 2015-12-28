9805ab17660cfcc29e376481ee1ddf53f29a16aa
Cesare de Cal <hi@cesare.io>
2015-12-28 08:48:55 +0100

    Added UI Interface

--- Chapter 9/notetoself.js
+++ Chapter 9/notetoself.js
@@ -1,10 +1,11 @@
 /* notetoself.js */
 
+window.onload = function() {
+	for (var i = 0; i < localStorage.length; i++) {
+		var key = localStorage.key(i);
+		var value = localStorage[key];
+	}	
+}
+
 localStorage.setItem("sticky_0", "Pick up dry cleaning");
 localStorage.setItem("sticky_1", "Cancel cable tv, who needs it now?");
-
-for (var i = 0; i < localStorage.length; i++) {
-	var key = localStorage.key(i);
-	var value = localStorage[key];
-	alert(value);
-}
