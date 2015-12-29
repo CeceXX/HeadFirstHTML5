e0d7637d0e7a5da7fdee8b19ec8071e2355ce7b0
Cesare de Cal <hi@cesare.io>
2015-12-29 12:20:57 +0100

    Used an array to avoid conflicts between localStorage localhost project

--- Chapter 9/notetoself.js
+++ Chapter 9/notetoself.js
@@ -3,14 +3,29 @@
 window.onload = function() {
 	var button = document.getElementById("add_button");
 	button.onclick = createSticky;
-	
-	for (var key in localStorage) {	
-		if (key.substring(0, 6) == "sticky") {
-			var value = localStorage.getItem(key);
-			addStickyToDOM(value);
-		}
-		var value = localStorage[key];
+
+	var stickiesArray = getStickiesArray();
+
+	for (var i = 0; i < stickiesArray.length; i++) {
+		var key = stickiesArray[i];
+		var value = stickiesArray[key];
+		addStickyToDOM(value);
 	}	
+}
+
+function createSticky() {
+	var value = document.getElementById("note_text").value;
+	if (value != "") {
+		var stickiesArray = getStickiesArray();		
+		var currentDate = new Date();
+		var key = "sticky_" + currentDate.getTime();
+		localStorage.setItem(key, value);
+		stickiesArray.push(key);
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+		addStickyToDOM(value);
+	} else {
+		alert("Insert something!");
+	}
 }
 
 function addStickyToDOM(value) {
@@ -23,16 +38,13 @@ function addStickyToDOM(value) {
 	stickies.appendChild(sticky);	
 }
 
-function createSticky() {
-	var value = document.getElementById("note_text").value;
-	if (value != "") {
-		var key = "sticky_" + localStorage.length;
-		localStorage.setItem(key, value);
-		addStickyToDOM(value);
+function getStickiesArray() {
+	var stickiesArray = localStorage.getItem("stickiesArray");
+	if (!stickiesArray) {
+		stickiesArray = [];
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
 	} else {
-		alert("Insert something!");
+		stickiesArray = JSON.parse(stickiesArray);
 	}
+	return stickiesArray;
 }
-
-localStorage.setItem("sticky_0", "Pick up dry cleaning");
-localStorage.setItem("sticky_1", "Cancel cable tv, who needs it now?");
